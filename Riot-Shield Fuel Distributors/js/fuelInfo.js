636d1abb6630e2fbd1f7b8f54d8a9310d77a75b0
Dayan Wishwanath <68376218+dayan-wishwanath@users.noreply.github.com>
2025-04-16 19:44:58 +0530

    Add files via upload

---
+++ Riot-Shield Fuel Distributors/js/fuelInfo.js
@@ -0,0 +1,21 @@
+document.getElementById('contactButton').addEventListener('click',function(){
+    alert('Contact Us button clicked!');
+});
+
+document.getElementById('fuelButton1').addEventListener('click',function(){
+    alert('Diesel More Details clicked!');
+});
+
+document.getElementById('fuelButton2').addEventListener('click',function(){
+    alert('Gasoline More Details clicked!');
+});
+document.getElementById('fuelButton3').addEventListener('click',function(){
+    alert('Ethanol E85 More Details clicked!');
+});
+document.getElementById('fuelButton4').addEventListener('click',function(){
+    alert('Diesel Exhaust Fluid More Details clicked!');
+});
+
+document.getElementById('fuelButton5').addEventListener('click',function(){
+    alert('Biodiesel More Details clicked!');
+});
